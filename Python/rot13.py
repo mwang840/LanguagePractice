eb85710a769811180df5c3aef6ab061a6f5bee93
mwang840 <maxwang@udel.edu>
2022-11-11 22:35:19 -0500

    rot13

---
+++ Python/rot13.py
@@ -0,0 +1,19 @@
+def rot13(message):
+    cypher = ""
+    for mess in message:
+        currMessage = ord(mess)
+        if(currMessage >= 97 and currMessage <= 123):
+            currMessage = ((currMessage  - 84) % 26) + 97
+        elif(currMessage >= 65 and currMessage <=91):
+            currMessage  = ((currMessage - 52) % 26) + 65  
+        cypher += chr(currMessage)
+    return cypher
+
+def main():
+    word = str(input("Enter a message"))
+    print("Word inputted in is: ", word)
+    print("Rot13 version of the word is: ", rot13(word))
+
+
+if __name__  == "__main__":
+    main()   
