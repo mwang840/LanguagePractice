5ef1deb51979fddc80ad2d028e5b7f321c626cd5
mwang840 <maxwang@udel.edu>
2022-12-17 20:04:16 -0500

    dashatize is done with the help of regex

--- TypeScript/dashatize.ts
+++ TypeScript/dashatize.ts
@@ -3,35 +3,14 @@ export const dashatize = (num: number)=>{
     if(isNaN(num)){
         return "NaN";
     }
-    //Negative Nums should be positive integers 
-    //Converts the number to a string array
-    const toString = Math.abs(num).toString();
-    let result = toString.split('');
-    let answer:string[] = [];
-    //console.log("Answer equals ", answer);
-    //console.log(result);
-    //Set up length
-    const length = toString.length;
-    //We will loop over the string and check to see if its odd and positions
-    for(let i = 0; i < length; i++){
-        //Check to see if we are AT the front of the string
-        //Checks to see if its indexes is even then add the dashes
-        if(i > 0 && Number(toString[i - 1]) % 2 == 0){
-            answer.push(`-${result[i]}`);
-        }
-        else if(i < length - 1 && Number(toString[i + 1]) % 2 == 0){
-            answer.push(`${result[i]}`);
-        }
-        else if(i > 0 && Number(toString[i - 1]) % 2 == 1){
-            answer.push(`-${result[i]}`);
-        }
-        else{
-            answer.push(`${result[i]}`);
-        }
+    else{
+        //One replace method
+        //Turns the num to a string, uses regex to check if every digit is a odd digit, then adds the dashes on both sides
+        //if there are multiple dashes, replace it with one dashatize
+        //if the dash is at the beginning, remove it
+        //if the dash is at the end, remove it
+        return num.toString().replace(/([13579])/g, '-$1-').replace(/\-+/g, "-").replace(/^\-/, '').replace(/\-$/,'');
     }
-    //checks if we are at the left end
-    //console.log(answer);
-    return answer.join("");
 };
 console.log(dashatize(1))
 console.log(dashatize(274));
