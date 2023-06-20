8d49ea6ab6131014d6858bc093ac0dd80bef1488
mwang840 <maxwang@udel.edu>
2023-06-19 21:28:44 -0600

    finished roman encoder

--- Ruby/RomanEncoder.rb
+++ Ruby/RomanEncoder.rb
@@ -1,33 +1,39 @@
 def romanNumEncoder(number)
-    romanNums = {1=>"I", 4=>"IV", 5=>"V", 9=>"IX", 10=>"X", 50=>"L", 100=>"C", 500=>"D", 1000=>"M"}
+    romanNums = {1000 => "M",
+    900 => "CM",
+    500 => "D",
+    400 => "CD",
+    100 => "C",
+    90 => "XC",
+    50 => "L",
+    40 => "XL",
+    10 => "X",
+    9 => "IX",
+    5 => "V",
+    4 => "IV",
+    1 => "I"}
     encoder = ""
     ##Logic for in case the number is exactly those criteria in the hash declared on line 2
-    if number == 1
-        puts "Number is one"
-        encoder = encoder + romanNums[1]
-    elsif number == 4
-        encoder = encoder + romanNums[4]   
-    elsif number == 5  
-        encoder = encoder + romanNums[5]
-    elsif number == 9
-        encoder =  encoder + romanNums[9]   
-    elsif number == 10
-        encoder =  encoder + romanNums[10]
-    elsif number == 50
-        encoder =  encoder + romanNums[50]  
-    elsif number == 100
-        encoder =  encoder + romanNums[100]  
-    elsif number == 500
-        encoder =  encoder + romanNums[500]  
-    elsif number == 1000
-        encoder =  encoder + romanNums[1000]                        
+    # for roman in romanNums
+    romanNums.each do |val, roman|
+        encoder << roman * (number / val)
+        number = number % val
     end
+   
     return encoder
 end
 
 
 def main()
     puts romanNumEncoder(1)
+    puts romanNumEncoder(4)
+    puts romanNumEncoder(6)
+    puts romanNumEncoder(14)
+    puts romanNumEncoder(21)
+    puts romanNumEncoder(89)
+    puts romanNumEncoder(91)
+    puts romanNumEncoder(984)
+    puts romanNumEncoder(1000)
 end
 
 main()
