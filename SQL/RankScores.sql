5534cd6e952422d87d95046a77478227037a8805
mwang840 <maxwang@udel.edu>
2023-08-21 14:06:47 -0400

    rank done

---
+++ SQL/RankScores.sql
@@ -0,0 +1,13 @@
+Create table If Not Exists Scores (id int, score DECIMAL(3,2))
+Truncate table Scores
+insert into Scores (id, score) values ('1', '3.5')
+insert into Scores (id, score) values ('2', '3.65')
+insert into Scores (id, score) values ('3', '4.0')
+insert into Scores (id, score) values ('4', '3.85')
+insert into Scores (id, score) values ('5', '4.0')
+insert into Scores (id, score) values ('6', '3.65')
+
+
+SELECT score, DENSE_RANK() OVER (ORDER BY score DESC) as `rank`
+FROM Scores
+ORDER BY score DESC;
