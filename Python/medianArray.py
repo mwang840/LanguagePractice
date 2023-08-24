b15ee71955e119f929d3b4c89c6cc725c64d40b9
mwang840 <maxwang@udel.edu>
2023-08-24 16:03:45 -0400

    hard problem done

--- Python/medianArray.py
+++ Python/medianArray.py
@@ -1,34 +1,47 @@
-def findMedianSortedArrays(nums1: list[int], nums2: list[int]) -> float:
+import math
+def findMedianSortedArrays(self,nums1: list[int], nums2: list[int]) -> float:
+        #call my helper function to merge the two arrays
         combinedArray = mergeArrays(nums1, nums2)
-        countSort(combinedArray)
-        middlePt = int(len(combinedArray) / 2) - 1
-        median = 0.0
-        if middlePt % 2 == 1:
-            median = combinedArray[middlePt]
-        elif middlePt % 2 == 0:
-            median = (combinedArray[middlePt] + combinedArray[middlePt + 1]) / 2  
+        #after the arrays are merged, use the counting sort function
+        mergeSort(combinedArray)
+        median = 0
+        #Conditionals: If the length of the combined array is even return the sum of the index (middle pt) plus its point after
+        if len(combinedArray) % 2 == 0:
+            median = (combinedArray[round(len(combinedArray) // 2) - 1] + combinedArray[round(len(combinedArray) // 2)]) / 2
+        #If it is odd, just return the middle of the array
+        elif len(combinedArray) % 2 == 1:
+               median = combinedArray[math.floor(len(combinedArray) // 2)]        
         return median
 
 ##helper function to merge two arrays
 def mergeArrays(nums1: list[int], nums2: list[int])-> list[int]:
         return nums1 + nums2
 
-#helper function to sort the merged array (counting sort)
-def countSort(numbers: list[int]):
-       totalSize = len(numbers)
-       resultingOutput = [0] * totalSize
-       initialCount = [0] * 10
-       for i in range(0, totalSize):
-              initialCount[numbers[i]] += 1
-       for i in range(1, 10):
-              initialCount[i] += initialCount[i - 1]
-       i = totalSize - 1
-       while i >= 0:
-              resultingOutput[initialCount[numbers[i]] - 1] = numbers[i]
-              initialCount[numbers[i]] -= 1
-              i -=1
-       for i in range(0, totalSize):
-              numbers[i] = resultingOutput[i]
+#helper function to sort the merged array (merge sort)
+def mergeSort(numbers: list[int]):
+      if(len(numbers)) > 1:
+            middlept = len(numbers) // 2
+            leftHalf =  numbers[:middlept]
+            rightHalf = numbers[middlept:]
+            mergeSort(leftHalf)
+            mergeSort(rightHalf)
+            i = j = k = 0
+            while i < len(leftHalf) and j < len(rightHalf):
+                  if leftHalf[i] < rightHalf[j]:
+                        numbers[k] = leftHalf[i]
+                        i +=1
+                  else:
+                        numbers[k] = rightHalf[j]
+                        j+=1
+                  k += 1
+            while i < len(leftHalf):
+                  numbers[k] = leftHalf[i]
+                  i += 1
+                  k += 1
+            while j < len(rightHalf):
+                  numbers[k] = rightHalf[j]
+                  j +=1 
+                  k +=1    
               
 def main():
     # print(mergeArrays([1, 3], [2]))
