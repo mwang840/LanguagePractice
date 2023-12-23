0e510fdf82ab968b3a20e8340c5f576b43764f04
Maxwell Wang <maxwang@udel.edu>
2023-12-22 20:36:38 -0500

    merge list completed

--- C++/BinaryTreeCameras.cpp
+++ C++/BinaryTreeCameras.cpp
@@ -14,25 +14,38 @@ using namespace std;
 
 
 struct TreeNode {
-       int val;
-    TreeNode *left;
+    int val;
+     TreeNode *left;
      TreeNode *right;
      TreeNode() : val(0), left(nullptr), right(nullptr) {}
      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
       TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  };
 
- int minCameraCover(TreeNode *root){
-    int totalCameras = 0;
-    if(root == nullptr){
+int totalCameras = 0;
+int minCameraCover(TreeNode *root);
+
+
+
+int searchCamera(TreeNode *root){
+     if(root == NULL){
         return 1;
     }
-    else{
-        int left = minCameraCover(root->left);
-        int right = minCameraCover(root->right);
-        if(left == 1 && right == 1){
-            return 2;
-        }
+    int left = minCameraCover(root->left);
+    int right = minCameraCover(root->right);
+    if(left == 0 || right == 0){
+        totalCameras++;
+        return 2;
+    }
+    if(left == 1 && right == 1){
+        return 0;
+    }
+}
+
+int minCameraCover(TreeNode *root){
+    int totalNodes = searchCamera(root);
+    if(totalNodes == 0){
+        return totalCameras + 1;
     }
     return totalCameras;
  }
